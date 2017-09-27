92a20eb0b77db1f736410bbe1afea1898d60f3a8
Ralf Jung <jung@mpi-sws.org>
2017-09-27 17:57:12 +0200

    update build system; support Coq 8.7

--- theories/peritem.v
+++ theories/peritem.v
@@ -47,7 +47,7 @@ Section proofs.
   Lemma new_bag_spec:
     {{{ True }}} new_stack #() {{{ s, RET #s; bag_inv s }}}.
   Proof.
-    iIntros (Φ) "HΦ". iApply wp_fupd.
+    iIntros (Φ) "_ HΦ". iApply wp_fupd.
     wp_seq. wp_bind (ref NONE)%E. wp_alloc l as "Hl".
     wp_alloc s as "Hs".
     iAssert ((∃ xs, is_bag_R N R xs s))%I with "[-HΦ]" as "Hxs".
