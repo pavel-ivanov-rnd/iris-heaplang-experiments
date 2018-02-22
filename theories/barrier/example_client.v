6cfb76f6a50a461ca62796d89f15626e6f8f5b15
Jacques-Henri Jourdan <jacques-henri.jourdan@normalesup.org>
2018-02-22 10:58:06 +0100

    Bump iris, fix build.

--- theories/barrier/example_client.v
+++ theories/barrier/example_client.v
@@ -61,7 +61,7 @@ Section ClosedProofs.
 
 Let Σ : gFunctors := #[ heapΣ ; barrierΣ ; spawnΣ ].
 
-Lemma client_adequate σ : adequate client σ (λ _, True).
+Lemma client_adequate σ : adequate NotStuck client σ (λ _, True).
 Proof. apply (heap_adequacy Σ)=> ?. apply client_safe. Qed.
 
 End ClosedProofs.
